05e26a4cdeac29c7330dc01e7f8cdb1095de37f3
Aleksandr Morozov <moroz.cuporos@gmail.com>
2023-11-07 11:24:59 +0300

    Rewriting application code without XJS syntax

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,3 +1,4 @@
+import { createElement } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,24 +6,42 @@ function App() {
 	const currentYear =
 		new Date().getFullYear(); /* Императивный стиль. Создаётся переменная и ей присваивается значение. */
 	return (
-		/* Начинается декларативная часть*/
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{currentYear}</p>
-			</header>
-		</div>
+		/* Начинается декларативная часть, если считать функции (createElement) признаком декларативного стиля*/
+		createElement(
+			'div',
+			{
+				className: 'App',
+			},
+			createElement(
+				'header',
+				{
+					className: 'App-header',
+				},
+				createElement('img', {
+					src: logo,
+					className: 'App-logo',
+					alt: 'logo',
+				}),
+				createElement(
+					'p',
+					null,
+					'Edit ',
+					createElement('code', null, 'src/App.js'),
+					' and save to reload.',
+				),
+				createElement(
+					'a',
+					{
+						className: 'App-link',
+						href: 'https://reactjs.org',
+						target: '_blank',
+						rel: 'noopener noreferrer',
+					},
+					'Learn React',
+				),
+				createElement('p', null, currentYear),
+			),
+		)
 	);
 }
 
